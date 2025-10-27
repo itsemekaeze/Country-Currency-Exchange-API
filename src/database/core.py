e0bca4bd455e261db46ebf89662e6eef38250a47
itsemekaeze <itsemekaeze903@gmail.com>
2025-10-27 19:58:13 +0100

    add changes in the core file

--- src/database/core.py
+++ src/database/core.py
@@ -6,8 +6,14 @@ import os
 
 load_dotenv()
 
+user = os.getenv('DATABASE_USERNAME')
+password = os.getenv('DATABASE_PASSWORD')
+host = os.getenv('DATABASE_HOSTNAME')
+dbname = os.getenv('DATABASE_NAME')
 
-SQLALCHEMY_DATABASE_URL_FORMAT = f"postgresql://{os.getenv("DATABASE_USERNAME")}:{os.getenv("DATABASE_PASSWORD")}@{os.getenv("DATABASE_HOSTNAME")}/{os.getenv("DATABASE_NAME")}"
+SQLALCHEMY_DATABASE_URL_FORMAT = f"postgresql://{user}:{password}@{host}/{dbname}"
+# SQLALCHEMY_DATABASE_URL_FORMAT = f"postgresql://{os.getenv('DATABASE_USERNAME')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOSTNAME')}/{os.getenv('DATABASE_NAME')}"
+
 
 SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL_FORMAT
 
